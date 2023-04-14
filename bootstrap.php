3ed40a928e1c8f8a6ba62e66ff5696627c8fd668
Evan Mullins <evanrm@gmail.com>
2023-04-14 14:36:06 -0400

    require_once files

--- bootstrap.php
+++ bootstrap.php
@@ -15,6 +15,7 @@ if ( function_exists( 'add_action' ) ) {
 					'name'     => 'ctb',
 					'label'    => __( 'ctb', 'newfold-ctb-module' ),
 					'callback' => function ( Container $container ) {
+						require_once __DIR__ . '/includes/Ctb.php';
 						new CTB( $container );
 					},
 					'isActive' => true,
