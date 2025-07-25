f10780fb71eb440547ff821b39490c377a4cc6b5
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-07-25 12:26:24 +0100

    include wp-module-link-tracker

--- static/ctb.js
+++ static/ctb.js
@@ -46,7 +46,7 @@
 				url.searchParams.set( 'id', ctbId );
 				const iframeURL = url.toString();
 				const iframe = document.createElement( 'iframe' );
-				iframe.src = iframeURL + '&locale=' + locale;
+				iframe.src = window.NewfoldRuntime.linkTracker.addUtmParams(iframeURL + '&locale=' + locale);
 				modalWindow.replaceChild( iframe, modalLoader );
                 ctbClickEvent( e, ctbId );
             } catch ( error ) {
@@ -97,7 +97,7 @@
 					const locale =
 						window.NewfoldRuntime?.sdk?.locale || 'en_US';
 					const iframeURL = data.url + '&locale=' + locale;
-					iframe.src = iframeURL;
+					iframe.src = window.NewfoldRuntime.linkTracker.addUtmParams(iframeURL);
 					modalWindow.replaceChild( iframe, modalLoader );
 					setTokenCookie( 'nfd_global_ctb_url_token', iframeURL, 25 );
 					// Track click event
@@ -118,7 +118,7 @@
 
 					// Fall back to opening destination URL
 					window.open(
-						destinationUrl,
+                        window.NewfoldRuntime.linkTracker.addUtmParams(destinationUrl),
 						'_blank',
 						'noopener noreferrer'
 					);
